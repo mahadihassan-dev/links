c911a54609d5e9838c63af02c100a55c441fc951
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2022-04-11 17:59:55 +0600

    Slider and Section Update

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.review_slider').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+    $('.banner_offer_card_slider').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        autoplay: false,
+        prevArrow: '<i class="fal fa-chevron-left banner_slide_btn banner_slide_prev_btn"></i>',
+        nextArrow: '<i class="fal fa-chevron-right banner_slide_btn banner_slide_next_btn"></i>',
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+});
