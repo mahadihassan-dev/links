e08d29a6a0210799ebc0aa766e684cddd12cc194
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2022-04-13 17:32:20 +0600

    New Function Added

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,7 +20,24 @@ $(document).ready(function() {
         slidesToScroll: 1,
     });
     // Filter Button
-    $("#filter_toggle_btn").click(function() {
-        $("#filter_box_card").toggle(250);
-    });
+    // $("#filter_toggle_btn").click(function() {
+    //     $("#filter_box_card").toggle(250);
+    // });
 });
+
+// Filter Button Custom Functions
+let toggleBtn = document.getElementById('filter_toggle_btn');
+let FilterFiled = document.getElementById('filter_box_card');
+
+function FilterToggleButton() {
+    FilterFiled.classList.toggle('FilterHide');
+    FilterFiled.style.transition = 'all linear .3s';
+}
+// BlockURLList Button Custom Functions
+let URLtoggleBtn = document.getElementById('blockURLToggle_Button');
+let URLField = document.getElementById('blockURLListBox');
+
+function blockURLToggleButton() {
+    URLField.classList.toggle('FilterHide');
+    URLField.style.transition = 'all linear .3s';
+}
