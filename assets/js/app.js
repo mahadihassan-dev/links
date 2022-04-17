a7db91c5e2127d524e5b002f7175805c6a996d19
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2022-04-17 23:31:15 +0600

    New Hover Added with Filter JS Function

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,12 +12,39 @@ $(document).ready(function() {
         dots: true,
         arrows: true,
         infinite: true,
-        autoplay: true,
+        autoplay: false,
         prevArrow: '<i class="fal fa-chevron-left banner_slide_btn banner_slide_prev_btn"></i>',
         nextArrow: '<i class="fal fa-chevron-right banner_slide_btn banner_slide_next_btn"></i>',
         speed: 300,
         slidesToShow: 1,
         slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
     });
     // Filter Button
     // $("#filter_toggle_btn").click(function() {
