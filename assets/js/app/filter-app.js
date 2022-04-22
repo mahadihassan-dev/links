ba957c61d3d3590bf8bc2ab6437a1eb5e2ec1776
Mahedi Hassan <81403638+mahadihassan-dev@users.noreply.github.com>
2022-04-22 12:23:03 +0600

    Responsive Update

--- assets/js/app/filter-app.js
+++ assets/js/app/filter-app.js
@@ -1,3 +1,15 @@
+/*
+ *   App-Name: Filter App
+ *   Version: 1
+ *   Author: Mahadi Hassan
+ *   Author URI: https://mahedihassan.com
+ *   This app is Under GPL and MIT License. All rights are Reserved by Mahedi Hassan.
+ *   This App Script is Fully Free to use. You can use it's Script Fully Free.
+ *   Please Use Author Attribute. and Help Author to get Coffee.
+ *   
+ *   DO NOT COPY THIS. AND USE YOUR NAME AS AUTHOR.
+ */
+"use strict"
 // Filter Button Custom Functions
 let toggleBtn = document.getElementById('filter_toggle_btn');
 let FilterFiled = document.getElementById('filter_box_card');
@@ -14,3 +26,7 @@ function blockURLToggleButton() {
     URLField.classList.toggle('FilterHide');
     URLField.style.transition = 'all linear .3s';
 }
+
+// Selected Row
+
+let SelectedKwFiled_boxField = document.getElementById('SelectedKwFiled_boxFiled');
